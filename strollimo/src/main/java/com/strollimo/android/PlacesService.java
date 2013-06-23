8e550928c290a1c950f2183f5d8de39004016d71
Balazs Balazs <balazs.balazs80@gmail.com>
2013-06-23 16:29:37 +0100

    budapest, ver1

--- strollimo/src/main/java/com/strollimo/android/PlacesService.java
+++ strollimo/src/main/java/com/strollimo/android/PlacesService.java
@@ -13,12 +13,15 @@ public class PlacesService {
     public PlacesService(Context context) {
         mContext = context;
         mPlaces = new HashMap<Integer, Place>();
-        mPlaces.put(1, new Place(1, "Lost in time", 51.504055, -0.019859, "id:1", context.getResources().getDrawable(R.drawable.canary2)));
-        mPlaces.put(2, new Place(2, "The mystery of the Bridge", 51.501757, -0.020514, "id:2", context.getResources().getDrawable(R.drawable.canary3)));
-        mPlaces.put(3, new Place(3, "The hidden 'Canary'", 51.507040, -0.022413, "id:3", context.getResources().getDrawable(R.drawable.canary4)));
-        mPlaces.put(4, new Place(4, "Amsterdam", 51.494996, -0.01649, "id:4", context.getResources().getDrawable(R.drawable.dock)));
-        mPlaces.put(5, new Place(5, "Floating Chinese", 51.49708, -0.016147, "id:5", context.getResources().getDrawable(R.drawable.lotus)));
-        mPlaces.put(6, new Place(6, "The Golden Egg", 51.505722, -0.027047, "id:6", context.getResources().getDrawable(R.drawable.westferry_circus)));
+
+
+        mPlaces.put(1, new Place(1, "Szabadsag bridge", 47.48586, 19.054885, "id:1", context.getResources().getDrawable(R.drawable.szabadsag_bridge)));
+        mPlaces.put(2, new Place(2, "Gellert square", 47.4847, 19.052868, "id:2", context.getResources().getDrawable(R.drawable.gellert_square2)));
+        mPlaces.put(3, new Place(3, "Gellért hill - Statue of Liberty", 47.486759, 19.047933, "id:3", context.getResources().getDrawable(R.drawable.gellert_hill)));
+        mPlaces.put(4, new Place(4, "Great Market Hall", 47.486904, 19.05864, "id:4", context.getResources().getDrawable(R.drawable.great_market_hall)));
+        mPlaces.put(5, new Place(5, "National Museum", 47.49121, 19.062696, "id:5", context.getResources().getDrawable(R.drawable.national_museum)));
+        mPlaces.put(6, new Place(6, "Kalvin square", 47.48992, 19.061687, "id:6", context.getResources().getDrawable(R.drawable.kalvin_square)));
+        mPlaces.put(7, new Place(7, "Raday Street, Budapest's Soho", 47.487354, 19.063082, "id:7", context.getResources().getDrawable(R.drawable.raday_street)));
     }
 
     public Place getPlaceById(int id) {
